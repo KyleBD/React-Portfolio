6a59458c0f0ee4a2a5ee1cd42e80e66abf127ce9
KyleBD <kylebradleydyer15@outlook.com>
2024-02-09 23:50:07 -0500

    Creating links in sidebar to my linkedin and github

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/K.png'
 import LogoSubtitle from '../../assets/images/logo1.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
 <div className='nav-bar'>
@@ -22,6 +23,26 @@ const Sidebar = () => (
             <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
         </NavLink>
     </nav>
+    <ul>
+        <li> 
+            <a
+                target='_blank' 
+                rel ='noreferrer'
+                href='https://www.linkedin.com/in/kylebradleydyer/'
+            >
+                <FontAwesomeIcon icon = {faLinkedin} color='#4d4d4e' />
+            </a>
+        </li>
+        <li>
+            <a
+                target='_blank' 
+                rel ='noreferrer'
+                href='https://github.com/KyleBD'
+            >
+                <FontAwesomeIcon icon = {faGithub} color='#4d4d4e' />
+            </a>
+        </li>
+    </ul>
 </div>
     )
 
