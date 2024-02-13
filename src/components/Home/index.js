59da1f8fa04ea860759d782b7dce6a6a1d964e5d
KyleBD <kylebradleydyer15@outlook.com>
2024-02-13 14:56:25 -0500

    Updated index file for animating letters.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,30 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import {Link} from 'react-router-dom'
 import './index.scss'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
-
+    const [letterClass, setLetterClass] = useState('text-enimate')
+    const nameArray = ['y', 'l', 'e']
+    const jobArray = ['c', 'o', 'm', 'p', 'u', 't', 'e', 'r', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r', 'i', 'n', 'g', ' ', 's', 't', 'u', 'd', 'e', 'n', 't', '.']
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm 
+                <h1>
+                <span className={letterClass}>H</span> 
+                <span className={`${letterClass} _12`}>i,</span>
+                <br /> 
+                <span className={`${letterClass} _13`}>I</span> 
+                <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt="developer" />
-                Kyle
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}/>
                 <br />
-                Aspiring Software Engineer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}/>
                 </h1>
                 <h2>Computer Engineer Student @ University of Toronto</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
