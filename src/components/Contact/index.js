7161a637795bf8530fd66c3292cfe6b6e153e2d7
KyleBD <kylebradleydyer15@outlook.com>
2024-02-29 23:12:44 -0500

    Created text boxes on contaact me page.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,6 +25,27 @@ const Contact = () => {
                 Please feel free to reach out if you are interested in learning more about any of the projects that
                 I am working on or if we are passionate about similar things!
             </p>
+            <div className='contact-form'>
+                <form>
+                    <ul>
+                        <li className='half'>
+                            <input type='text' name='name' placeholder='Name' required/>
+                        </li>
+                        <li className='half'>
+                            <input type='email' name='email' placeholder='Email' required/>
+                        </li>  
+                        <li>
+                            <input type='Subject' name='text' placeholder='subject' required/>
+                        </li>
+                        <li>
+                            <textarea placeholder='Message' name='message' required></textarea>
+                        </li>   
+                        <li>
+                            <input type='submit' className='flat-button' value='SEND'/>
+                        </li>                                 
+                    </ul>
+                </form>
+            </div>
             </div>
         </div>
         <Loader type='pacman'/>
